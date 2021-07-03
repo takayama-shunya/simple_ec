f826ae55077c8904ae48c9021686b583ecbe44ca
takayama shunya <katonbo1@icloud.com>
2021-07-03 21:27:46 +0900

    ShopsTest追記

--- tests/Feature/ShopsTest.php
+++ tests/Feature/ShopsTest.php
@@ -18,23 +18,61 @@ class ShopsTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_url()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
 
+    // public function set_user()
+    // {
+    //     return User::factory()->create();
+    // }
+
     public function test_stock_count()
     {
-        $user = User::factory()->create();
-        // Laravel8以降、Globalなfactory関数は廃止され、Modelから呼び出す形となりました
-        Stock::factory()
-            ->count(10)
-            ->for($user)
-            ->create();
-
+        Stock::factory(10)->create();
         $this->assertDatabaseCount('stocks', 10);
+        $stock_1 = Stock::find(1);
+        $this->assertInstanceOf(Stock::class, $stock_1);
+        $stock_2 = Stock::find(1)->toArray();
+        $expected = [
+            'id', 'created_at', 'updated_at','name', 'detail', 'fee', 'imgpath', 'user_id', 
+        ];
+        $this->assertSame($expected, array_keys($stock_2));
+    }
+
+
+    public function test_stock_crud()
+    {
+        $date = [
+            'name' => 'テスト',
+            'detail' => 'CRUD機能テスト',
+            'fee' => 1000,
+            'imgpath' => "",    
+            'user_id' => User::factory(),
+        ];
+
+        $stock = Stock::factory()->create($date);
+        $this->assertDatabaseHas('stocks', [
+            'name' => 'テスト',
+        ]);
+
+        $update = [
+            'name' => 'updateテスト',
+        ];
+
+        $stock->update($update);
+        $this->assertDatabaseHas('stocks', [
+            'name' => 'updateテスト',
+        ]);
+
+        $stock->delete();
+        $this->assertDatabaseMissing('stocks', [
+            'name' => 'updateテスト',
+        ]);
+        $this->assertDeleted($stock);
 
     }
 }
